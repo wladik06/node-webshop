85b467c8955471e27cd5bb526147b5dd009f3b80
Vladimir <57994892+wladik06@users.noreply.github.com>
2021-12-19 04:22:06 +0100

    available products + create product

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-const express = require('express');
+const express = require("express");
+const bodyparser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("UCN Webshop");
+	res.render('index.ejs');
 });
 
+app.use(
+	bodyparser.urlencoded({
+		extended: true,
+	})
+);
+
+app.set("template engine", "ejs");
+
+app.use("/css", express.static(path.resolve(__dirname, "/css")));
+app.use("/js", express.static(path.resolve(__dirname, "/js")));
+
 app.listen(3000, () => {
-	console.log("Runs on http://localhost:$(3000)");
+	console.log("Runs on http://localhost:3000");
 });
